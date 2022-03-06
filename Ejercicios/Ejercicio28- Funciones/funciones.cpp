8d2791bb55e31364ae566595459855e28372637d
Valery Lemus <valery.lemus@unah.hn>
2022-03-06 17:25:37 -0600

    agregando ejercicio funciones

---
+++ Ejercicios/Ejercicio28- Funciones/funciones.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int sumar() {
+	return 5 + 7;
+}
+
+int main (int argc, char const *argv[])
+	{
+	  int resultado = sumar();
+	  
+	  cout << resultado;
+	
+	return 0;
+}
