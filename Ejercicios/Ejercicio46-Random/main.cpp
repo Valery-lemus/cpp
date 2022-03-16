2638600c597b42fa1b37a10bb47ed91916839e29
Valery Lemus <valery.lemus@unah.hn>
2022-03-16 00:27:15 -0600

    Agregando ejercicio 46 random

---
+++ Ejercicios/Ejercicio46-Random/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+int numero = 0;
+
+//inicializa el numero random
+srand(time(NULL));
+
+ for (int i = 0; i < 20; i++)
+ {
+
+    //genera un numero entre 1 y 10
+    numero = rand() % 10 + 1;
+    cout << " Primer Numero al azar: "<< numero;
+    cout << endl;
+  }
+
+return 0;
+
+}
