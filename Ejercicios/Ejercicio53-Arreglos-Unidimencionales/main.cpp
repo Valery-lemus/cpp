0c16b718383f73a603a78fb0ebe09bfc6328682c
Valery Lemus <valery.lemus@unah.hn>
2022-04-19 19:00:09 -0600

    Arreglos unidimencionales

---
+++ Ejercicios/Ejercicio53-Arreglos-Unidimencionales/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main (int argc, char const *argv[])
+{
+	int numeros [5]; // declaracion del arreglo
+	
+	// asignacion de valores a un arreglo unidemencional
+	
+	numeros[0] = 10;
+	numeros[1] = 5;
+	numeros[2] = 7;
+	numeros[3] = 50;
+	numeros[4] = 2;
+	
+	//lecturas de los valores de un arreglo unidemencional
+	
+	cout << numeros[0] << endl;
+	cout << numeros[1] << endl;
+	cout << numeros[2] << endl;
+	cout << numeros[3] << endl;
+	cout << numeros[4] << endl;
+	
+	return 0;
+	
+}
